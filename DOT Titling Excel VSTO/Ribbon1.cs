78743751b853e326d737c91c16a81f5a818ff563
Patrick Handel <patrickhandel@gmail.com>
2017-08-08 18:24:46 -0500

    Connects to workbook data

--- DOT Titling Excel VSTO/Ribbon1.cs
+++ DOT Titling Excel VSTO/Ribbon1.cs
@@ -27,40 +27,93 @@ namespace DOT_Titling_Excel_VSTO
         }
         public static void MergeDataIntoNewFile()
         {
-            string summary = "This is the summary";
-            string JiraId = "DOTTITLNG-73";
-            string id = JiraId.Replace("DOTTITLNG-", string.Empty);
+            Excel.Worksheet activeWorksheet = Globals.ThisAddIn.Application.ActiveSheet;
+            Excel.Range activeCell = Globals.ThisAddIn.Application.ActiveCell;
+            Excel.Range selection = Globals.ThisAddIn.Application.Selection;
 
-            string template = @"C:\\Users\\patrick.handel\\Desktop\\MailMergeOut\\MyDoc.docx";
-            string newfile = @"C:\\Users\\patrick.handel\\Desktop\\Exported\\" + summary + " (" + id + ").docx";
-            File.Copy(template, newfile, true);
-
-            using (WordprocessingDocument wordDoc = WordprocessingDocument.Open(newfile, true))
+            if (activeCell != null)
             {
-                string docText = null;
-                using (StreamReader sr = new StreamReader(wordDoc.MainDocumentPart.GetStream()))
-                {
-                    docText = sr.ReadToEnd();
-                }
+                int row = activeCell.Row;
+                int jiraIDCol = 6;
 
-                docText = MergeField(docText, "Summary", summary);
-                docText = MergeField(docText, "DOTTITLNG", JiraId);
-                docText = MergeField(docText, "Epic", "Reset/Rollback - R2");
-                docText = MergeField(docText, "Release", "Release");
-                docText = MergeField(docText, "Sprint", "Sprint");
-                docText = MergeField(docText, "Story1", "Story1");
-                docText = MergeField(docText, "Story2", "Story2");
-                docText = MergeField(docText, "Story3", "Story3");
-                docText = MergeField(docText, "Description", "Description");
-                docText = MergeField(docText, "Web Services", "Web Services");
-                docText = MergeField(docText, "Date Approved", "12/15/2010");
-                docText = MergeField(docText, "Document Date", DateTime.Now.ToShortDateString());
 
-                using (StreamWriter sw = new StreamWriter(wordDoc.MainDocumentPart.GetStream(FileMode.Create)))
+
+                string JiraId = CellGetStringValue(activeWorksheet, row, jiraIDCol);
+                if (activeWorksheet.Name == "Stories" && JiraId.Substring(1, 11) == "DOTTITLING-")
                 {
-                    sw.Write(docText);
+                    int epicCol = 1;
+                    int summaryCol = 5;
+                    int releaseCol = 11;
+                    int sprintCol = 13;
+                    int story1Col = 30;
+                    int story2Col = 31;
+                    int story3Col = 32;
+                    int descriptionCol = 29;
+                    int webServicesCol = 33;
+                    int dateSubmittedCol = 27;
+                    int dateApprovedCol = 28;
+
+                    string summary = CellGetStringValue(activeWorksheet, row, summaryCol);
+                    string epic = CellGetStringValue(activeWorksheet, row, epicCol);
+                    string release = CellGetStringValue(activeWorksheet, row, releaseCol);
+                    string sprint = CellGetStringValue(activeWorksheet, row, sprintCol);
+                    string story1 = CellGetStringValue(activeWorksheet, row, story1Col);
+                    string story2 = CellGetStringValue(activeWorksheet, row, story2Col);
+                    string story3 = CellGetStringValue(activeWorksheet, row, story3Col);
+                    string description = CellGetStringValue(activeWorksheet, row, descriptionCol);
+                    string webServices = CellGetStringValue(activeWorksheet, row, webServicesCol);
+                    string dateSubmited = CellGetStringValue(activeWorksheet, row, dateSubmittedCol);
+                    string dateApproved = CellGetStringValue(activeWorksheet, row, dateApprovedCol);
+
+                    string id = JiraId.Replace("DOTTITLNG-", string.Empty);
+                    string template = @Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\MailMergeOut\\MyDoc.docx";
+                    string newfile = @Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\Exported\\" + summary + " (" + id + ").docx";
+                    File.Copy(template, newfile, true);
+
+                    using (WordprocessingDocument wordDoc = WordprocessingDocument.Open(newfile, true))
+                    {
+                        string docText = null;
+                        using (StreamReader sr = new StreamReader(wordDoc.MainDocumentPart.GetStream()))
+                        {
+                            docText = sr.ReadToEnd();
+                        }
+
+                        docText = MergeField(docText, "Summary", summary);
+                        docText = MergeField(docText, "DOTTITLNG", JiraId);
+                        docText = MergeField(docText, "Epic", epic);
+                        docText = MergeField(docText, "Release", release);
+                        docText = MergeField(docText, "Sprint", sprint);
+                        docText = MergeField(docText, "Story1", story1);
+                        docText = MergeField(docText, "Story2", story2);
+                        docText = MergeField(docText, "Story3", story3);
+                        docText = MergeField(docText, "Description", description);
+                        docText = MergeField(docText, "Web Services", webServices);
+                        docText = MergeField(docText, "Date Submitted", dateSubmited);
+                        docText = MergeField(docText, "Date Approved", dateApproved);
+                        docText = MergeField(docText, "Document Date", DateTime.Now.ToShortDateString());
+
+                        using (StreamWriter sw = new StreamWriter(wordDoc.MainDocumentPart.GetStream(FileMode.Create)))
+                        {
+                            sw.Write(docText);
+                        }
+                    }
                 }
             }
+        }
+
+        public static string CellGetStringValue(Excel.Worksheet sheet, int row, int column)
+        {
+            var result = string.Empty;
+
+            if (sheet != null)
+            {
+                var rng = sheet.Cells[row, column] as Excel.Range;
+
+                if (rng != null)
+                    result = (string)rng.Text;
+            }
+
+            return result;
         }
 
         private static String MergeField(string docText, string field, string newText)
@@ -73,13 +126,21 @@ namespace DOT_Titling_Excel_VSTO
         {
             Excel.Worksheet activeWorksheet = Globals.ThisAddIn.Application.ActiveSheet;
             Excel.Range activeCell = Globals.ThisAddIn.Application.ActiveCell;
-            Excel.Range selectedRange = Globals.ThisAddIn.Application.Selection;
+            Excel.Range selection = Globals.ThisAddIn.Application.Selection;
+
+            //Filtered selection
+            for (int rowIndex = selection.Row; rowIndex < selection.Row + selection.Rows.Count; rowIndex++)
+            {
+                if (activeWorksheet.Rows[rowIndex].EntireRow.Height != 0)
+                {
+                    string sval = activeWorksheet.Rows[rowIndex].Text;
+                }
+            }
 
             if (activeCell != null)
             {
                 string sValue = activeCell.Value2.ToString();
                 string sText = activeCell.Text;
-                System.Windows.Forms.MessageBox.Show(sText);
             }
         }
     }
