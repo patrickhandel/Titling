3dc3c98a70e2c7ae428f94640bc788a38bbe973d
Patrick Handel <patrickhandel@gmail.com>
2017-08-08 16:26:23 -0500

    Initial Commit

---
+++ DOT Titling Excel VSTO/Ribbon1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+using Excel = Microsoft.Office.Interop.Excel;
+using Office = Microsoft.Office.Core;
+using Microsoft.Office.Tools.Excel;
+
+namespace DOT_Titling_Excel_VSTO
+{
+    public partial class Ribbon1
+    {
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, RibbonControlEventArgs e)
+        {
+            Excel.Worksheet activeWorksheet = Globals.ThisAddIn.Application.ActiveSheet;
+            Excel.Range activeCell = Globals.ThisAddIn.Application.ActiveCell;
+            Excel.Range selectedRange = Globals.ThisAddIn.Application.Selection;
+
+            if (activeCell != null)
+            {
+                string sValue = activeCell.Value2.ToString();
+                string sText = activeCell.Text;
+                System.Windows.Forms.MessageBox.Show(sText);
+            }
+        }
+    }
+}
